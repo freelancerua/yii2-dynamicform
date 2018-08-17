bc479e6fad2447f70cff5518d3ad08043bf9b603
Dmytro S. <freelancerua@protonmail.com>
2018-08-17 18:54:18 +0300

    [*] - Update yiiForm call

--- src/assets/yii2-dynamic-form.js
+++ src/assets/yii2-dynamic-form.js
@@ -86,7 +86,7 @@
         });
 
         // remove "error/success" css class
-        var yiiActiveFormData = $('#' + widgetOptions.formId).data('yiiActiveForm');
+        var yiiActiveFormData = $('#' + widgetOptions.formId).yiiActiveForm('data');
         $template.find('.' + yiiActiveFormData.settings.errorCssClass).removeClass(yiiActiveFormData.settings.errorCssClass);
         $template.find('.' + yiiActiveFormData.settings.successCssClass).removeClass(yiiActiveFormData.settings.successCssClass);
 
